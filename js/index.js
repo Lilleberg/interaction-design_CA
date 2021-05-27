851e25d6f93f3af80d38497295660aab25d09b10
Maria Lilleberg Holm <maria.lilleberg@gmail.com>
2021-05-27 02:45:38 +0200

    workin progress

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 const cartButton = document.querySelector(".cart-icon");
 const cartContainer = document.querySelector(".cart-container");
+const addToCart = document.querySelectorAll(".add-to-cart");
+const cartItems = document.querySelector(".cart");
+
+let games = [];
+let gameObject = {};
 
 cartButton.onclick = function () {
   if (cartContainer.style.display === "block") {
@@ -9,3 +14,41 @@ cartButton.onclick = function () {
   }
 }
 
+addToCart.forEach(function (button) {
+  button.addEventListener("click", function () {
+
+    if (!games.length === 0) {
+      document.querySelector(".empty").style.display = "none";
+    } else {
+      document.querySelector(".empty").style.display = "block";
+    }
+    cartContainer.style.display = "block";
+
+
+    const price = event.target.dataset.price;
+    console.log(price);
+    let gameAdded = event.target.dataset.game;
+    games.push(gameAdded);
+    games.push(price);
+    console.log(games);
+    for (let i = 0; i < games.length; i++) {
+      cartItems.innerHTML += `${games[i]}`;
+    }
+
+  })
+});
+/*for (let i = 0; i < games.length; i++) {
+  cartContainer.style.display = "block";
+  document.querySelector(".empty").style.display = "none";
+  console.log(gameAdded);
+  addGameList.innerHTML += `${gameAdded}`;
+}
+}*/
+
+/*gameProducts.forEach(function (product) {
+  product.addEventListener("click", function () {
+    cartContainer.style.display = "block";
+    let gameAdded = event.target.dataset.game;
+    addGameList.innerHTML += `<li>${gameAdded}</li>`;
+  })
+});*/
